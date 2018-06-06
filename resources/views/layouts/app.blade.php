25e5893e9e5d81cf99bc4918aa35e3539bb9ded8
minako.yanagi <mnk14s@yahoo.co.jp>
2018-06-06 01:17:08 +0000

    revised

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,27 +12,6 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
-    <header>
-    <nav class="navbar navbar-inverse navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/">MessageBoard</a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('messages.create', '新規メッセージの投稿') !!}</li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    </header>
-
     <body>
         @include('commons.navbar')
         
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -4,7 +4,7 @@
 
     <h1>メッセージ一覧</h1>
 
-    @if (count($tasklists) > 0)
+    @if (count($messages) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -14,16 +14,16 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($tasklists as $tasklist)
+                @foreach ($messages as $message)
                     <tr>
-                        <td>{!! link_to_route('tasklists.show', $tasklist->id, ['id' => $tasklist->id]) !!}</td>
-                        <td>{{ $tasklist->title }}</td>
-                        <td>{{ $tasklist->content }}</td>
+                        <td>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!}</td>
+                        <td>{{ $message->title }}</td>
+                        <td>{{ $message->content }}</td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     @endif
-    {!! link_to_route('tasklists.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('messages.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
 
 @endsection
